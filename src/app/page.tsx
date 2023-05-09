2ff7774cc659e393522e829c02b6a927565f4f69
Ismael Bautista <ismaelbautista6@gmail.com>
2023-05-09 21:42:50 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           </video>
           <div className='content'>
             <div className='text-center mt-24'>
-              <h2 className="hero glitch layers" data-text="fallen"><span>FALLEN</span></h2>
+              <h2 className="hero glitch layers" data-text="FALLEN"><span>FALLEN</span></h2>
               <p className='text-large text-gray-100 uppercase tracking-widest'>
                 Per aspera <span className='text-gray-500'>ad astra</span> 
               </p>
